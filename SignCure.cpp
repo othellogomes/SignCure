bd6103f159d87ab14567986be57baddbd09e4e02
Othello D. Gomes <56773910+othellogomes@users.noreply.github.com>
2023-03-03 11:59:41 -0500

    Update SignCure.cpp

--- SignCure.cpp
+++ SignCure.cpp
@@ -30,7 +30,7 @@ cx_mat X = {{0, 1},
 cx_mat Y = { {0, -imaginary},
           {imaginary, 0} };
 cx_mat Z = {{1, 0},
-          {0, -1}};
+          {0, -1}}; /* Not initialising properly */
 cx_mat I(2, 2, fill::eye);
 
 cout << X << endl;
@@ -41,11 +41,45 @@ mat Z = {{1,0},{0,-1}};
 mat I = {{1,0},{0,1}};
 */        
 
-/*int H_1 = zeros(2^[n+m]); */ /*Initialising Hamiltonian*/
-
+int H = zeros({2^[n+m]}{2^[n+m]}); /*Initialising Hamiltonian*/
+  for (int i = 0; i < m-1; i++){
+    /*Construct H_C */
+    /*order qubits: 0....n-1 and n....n+m-1 */
+    
+    H_m = - (X + Z + I); 
+    if sign(inst[i][0] == -1){
+      S[0] = X;
+   }
+      else{
+        S[0] = Z;
+      }
+  }
+    
+  for (int i = 0; i < m-1; i++){
+    H_m = - (X + Z + I); 
+    if sign(inst[i][1] == -1){
+      S[1] = X;
+   }
+      else{
+        S[1] = Z;
+    }
+  }
+for (int i = 0; i < m-1; i++){
+    H_m = - (X + Z + I); 
+    if sign(inst[i][2] == -1){
+      S[2] = X;
+   }
+      else{
+        S[2] = Z;
+    }
+  }
+qubits = abs(inst[1][:])-1;
+  if 
 /*Intialise S for the if and else statements*/
 
 /* Compute Avg. Sign <S>*/
+  
+  
 
 return 0;
 }
