bdffbaa8a8e8cdad44c9b4bee40df594bc7bf36d
Othello D. Gomes <56773910+othellogomes@users.noreply.github.com>
2023-03-03 03:54:49 -0500

    Add files via upload

--- SignCure.cpp
+++ SignCure.cpp
@@ -1,6 +1,9 @@
+/* Othello D. Gomes
+Sign Curing Numerics
+*/
 #include <iostream>
 #include <armadillo> 
-
+#include <complex>  
 using namespace std;
 using namespace arma;
 
@@ -8,43 +11,41 @@ using namespace arma;
 
 int main()
 {
-m = 2; 
-n = 4;  
-inst = [m][3];
-for (int i = 0,1; i < m-1; i++) {
-  for (int j = 0,1,2; j++){
-  var = rand() % n + 1;
-  sign = rand() % 4 - 4;
+int m = 2; 
+int n = 4;  
+int inst [m][3];
+for (int i = 0; i < m-1; i++){
+  for (int j = 0; j <= 2; j++){
+  int var = rand() % n + 1;
+  int sign = rand() % 4 - 4;
   inst [i][j] = sign * var;
 }
 }
-
-
-
-
-
-
 /*Generate Hamiltonians in this part of the code*/
-
 /*Pauli Matrices: need to make sure they're dense matrices*/
+std::complex<double> imaginary(1);
 
-mat X = {{0, 1}{1, 0}};
-mat Y = {{0, -i}{i, 0}};          
-mat Z = {{1,0}{0,-1}};
-mat I = {{1,0}{0,1}}         
+cx_mat X = {{0, 1},
+          {1, 0}};
+cx_mat Y = { {0, -imaginary},
+          {imaginary, 0} };
+cx_mat Z = {{1, 0},
+          {0, -1}};
+cx_mat I(2, 2, fill::eye);
 
-Hamiltonian = zeros({2^{n+m}}{2^{n+m}}); /*Initialising Hamiltonian*/
+cout << X << endl;
 
+/*mat X = {{0, 1},{1, 0}};
+mat Y = {{0, eye},{eye, 0}};          
+mat Z = {{1,0},{0,-1}};
+mat I = {{1,0},{0,1}};
+*/        
 
+/*int H_1 = zeros(2^[n+m]); */ /*Initialising Hamiltonian*/
+
+/*Intialise S for the if and else statements*/
 
 /* Compute Avg. Sign <S>*/
 
-
-
-
-
-
-
-
-
+return 0;
 }
