59be366d8576c5ece3876e8f47761003890ed140
Othello D. Gomes <56773910+othellogomes@users.noreply.github.com>
2023-03-21 15:21:08 -0400

    Update SignCure.cpp

--- SignCure.cpp
+++ SignCure.cpp
@@ -1,8 +1,6 @@
 /* Othello D. Gomes
 Sign Curing Numerics
-
 Use c++14
-
 */
 #include <iostream>
 #include <armadillo> 
@@ -62,86 +60,134 @@ int main(){
   //Initialising Hamiltonians
   cx_mat HC(std::pow(2,n+m),std::pow(2,n+m));
   HC.fill(0.0+0.0i);
+  HC.brief_print();
 
-  return 0;
+
+  for (int i = 0; i < m-1; i++){
+    /*Construct H_C */
+    /*order qubits: 0....n-1 and n....n+m-1 */
+std::complex<double> qubits_n[1 << n]; // Qubits for indices 0 to n-1
+std::complex<double> qubits_m[1 << m + n]; // Qubits for indices n to n+m-1
+    
+    cx_mat H_m = - (X + Z + I); 
+    if (sign(inst[i][0] == -1)){
+      S[0] = X;
+   }
+  else{
+        S[0] = Z;
+      }
+  }
+    
+  for (int i = 0; i < m-1; i++){
+    cx_mat H_m = - (X + Z + I); 
+    if sign(inst[i][1] == -1){
+      S[1] = X;
+   }
+      else{
+        S[1] = Z;
+    }
+  }
+for (int i = 0; i < m-1; i++){
+    H_m = - (X + Z + I); 
+    if sign(inst[i][2] == -1){
+      S[2] = X;
+   }
+      else{
+        S[2] = Z;
+    }
+  }
+int qubits = abs(inst[1][:])-1;
+  if (qubits = 0){
+    cx_mat H_k = S[]
+  }
+  else{
+    cx_mat H_k = I; /* */
+  }
+  for (int j = 1; j < n-1; j++){
+    if(j=qubits){
+      cx_mat H_k = kron(H_k,S)  
+    }
+    else{
+      cx_mat H_k = kron(H_k, I)
+    }
+  }
+for (int j = n; j < n-m+1; j++){
+  if(j-n == i){
+    cx_mat H_k = kron(H_k, H_m)
+  }
+  else{
+    cx_mat H_k = kron(H_k, I)
+  }
 }
-  /*mat X = {{0, 1},{1, 0}};
-  mat Y = {{0, eye},{eye, 0}};          
-  mat Z = {{1,0},{0,-1}};
-  mat I = {{1,0},{0,1}};
-  */        
+cx_mat H = HC + H_k; 
 
-  /*int H_1 = zeros(2^[n+m]); */ /*Initialising Hamiltonian*/
+return 0; 
+}
+//  
+//     /*Construct H_C */
+//     /*order qubits: 0....n-1 and n....n+m-1 */
+    
+//     H_m = - (X + Z + I); 
+//     if sign(inst[i][0] == -1){
+//       S[0] = X;
+//    }
+//   else{
+//         S[0] = Z;
+//       }
+//   }
+    
+//   for (int i = 0; i < m-1; i++){
+//     int H_m = - (X + Z + I); 
+//     if sign(inst[i][1] == -1){
+//       S[1] = X;
+//    }
+//       else{
+//         S[1] = Z;
+//     }
+//   }
+// for (int i = 0; i < m-1; i++){
+//     H_m = - (X + Z + I); 
+//     if sign(inst[i][2] == -1){
+//       S[2] = X;
+//    }
+//       else{
+//         S[2] = Z;
+//     }
+//   }
+// int qubits = abs(inst[1][:])-1;
+//   if (qubits = 0){
+//     H_k = S[]
+//   }
+//   else{
+//     H_k = I; /* */
+//   }
+//   for (int j = 1; j < n-1; j++){
+//     if(j=qubits){
+//       H_k = kron(H_k,S)  
+//     }
+//     else{
+//       H_k = kron(H_k, I)
+//     }
+//   }
+// for (int j = n; j < n-m+1; j++){
+//   if(j-n == i){
+//     H_k = kron(H_k, H_m)
+//   }
+//   else{
+//     H_k = kron(H_k, I)
+//   }
+// }
+// H = H + H_k; 
+// /*Intialise S for the if and else statements*/
 
-  /*Intialise S for the if and else statements*/
+// /* Compute Avg. Sign <S>*/
 
-  // int H = zeros({2^[n+m]}{2^[n+m]}); /*Initialising Hamiltonian*/
-  /*Construct H_C */
-  //   for (int i = 0; i < m; i++){
-  //     
-  //     /*order qubits: 0....n-1 and n....n+m-1 */
-      
-  //     H_m = - (X + Z + I); 
-  //     if sign(inst[i][0] == -1){
-  //       S[0] = X;
-  //    }
-  //   else{
-  //         S[0] = Z;
-  //       }
-  //   }
-      
-  //   for (int i = 0; i < m-1; i++){
-  //     int H_m = - (X + Z + I); 
-  //     if sign(inst[i][1] == -1){
-  //       S[1] = X;
-  //    }
-  //       else{
-  //         S[1] = Z;
-  //     }
-  //   }
-  // for (int i = 0; i < m-1; i++){
-  //     H_m = - (X + Z + I); 
-  //     if sign(inst[i][2] == -1){
-  //       S[2] = X;
-  //    }
-  //       else{
-  //         S[2] = Z;
-  //     }
-  //   }
-  // int qubits = abs(inst[1][:])-1;
-  //   if (qubits = 0){
-  //     H_k = S[]
-  //   }
-  //   else{
-  //     H_k = I; /* */
-  //   }
-  //   for (int j = 1; j < n-1; j++){
-  //     if(j=qubits){
-  //       H_k = kron(H_k,S)  
-  //     }
-  //     else{
-  //       H_k = kron(H_k, I)
-  //     }
-  //   }
-  // for (int j = n; j < n-m+1; j++){
-  //   if(j-n == i){
-  //     H_k = kron(H_k, H_m)
-  //   }
-  //   else{
-  //     H_k = kron(H_k, I)
-  //   }
-  // }
-  // H = H + H_k; 
-  // /*Intialise S for the if and else statements*/
+// int eH = expmat(-H);
 
-  // /* Compute Avg. Sign <S>*/
+// int avgSign = (trace(eH)/trace(abs(eH)));
 
-  // int eH = expmat(-H);
-
-  // int avgSign = (trace(eH)/trace(abs(eH)));
-
-  // /* Compute Avg. Sign <S>*/
+// /* Compute Avg. Sign <S>*/
 
 
-  // return 0;
-  // }
+// return 0;
+// }
